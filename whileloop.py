14efcbc282ee19d3875d897d39c37f0e3c14f278
kotachandrikha <chandrikakota1926@gmail.com>
2023-07-04 16:49:53 +0530

    first commit

---
+++ whileloop.py
@@ -0,0 +1,23 @@
+
+#name=""
+#while name!="chandu":
+#    name=input("enter name to authenticate:")
+#print("hello chandu thank you for authenticating")
+
+#username and password
+
+username=""
+password=""
+while username!="chandrika" and password!=123:
+    username=input("enter correct username:")
+    password=input("enter the correct passwor:")
+print("thank you or authenticating")
+
+
+#username=""
+#password=""
+#while username=="chandrika" and password==123:
+#    username=input("enter correct username:")
+#    password=input("enter the  correct passwor:")
+#print("thank you for authenticating")
+
