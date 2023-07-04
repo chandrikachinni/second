5f5dca133476d394597bc53bf415ae25fd872b3b
kotachandrikha <chandrikakota1926@gmail.com>
2023-07-04 15:21:39 +0530

    first commit

---
+++ #nested loop.py
@@ -0,0 +1,38 @@
+
+#nested or loop
+
+#for i in range(1,11):
+#   for j in range(1,11):
+#        print(i*j,end=:"")
+#    print()
+
+
+# x=int(input("enter x value:"))
+# y=int(input("enter y value"))
+# for i in range(1,x):
+#     for j in range(1,y):
+#         print(i*j,end="")
+#     print()
+
+
+#right triangle pattern
+
+for i in range(1,6):
+    for j in range(1,i+1):
+        print("*",end="")
+    print()
+
+
+    #rectangular pattern
+    #r=int(input("enter no of rows:"))
+    #c=int(inpt("enter no of colums:"))
+    #for i in range(r):
+    #     for j in range(c):
+    #         print("*",end="")
+    #    print()
+    
+
+
+
+
+
